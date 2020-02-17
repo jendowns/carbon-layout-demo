91dc9bfdbaf171e0a1cc23dd94e10fc46a715a2e
Jen Downs <hello@jendowns.com>
2020-02-17 14:03:19 -0600

    Updates

---
+++ precache-manifest.2260f550da551ce3505f5f32e82702ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "465ba28d956732394cbe",
+    "url": "/modular-layouts/static/js/runtime~main.b6654ee1.js"
+  },
+  {
+    "revision": "d754035e60af7cfef318",
+    "url": "/modular-layouts/static/js/main.5a32fd1b.chunk.js"
+  },
+  {
+    "revision": "b3824466939d07301ced",
+    "url": "/modular-layouts/static/js/2.2a786427.chunk.js"
+  },
+  {
+    "revision": "d754035e60af7cfef318",
+    "url": "/modular-layouts/static/css/main.20b22b0f.chunk.css"
+  },
+  {
+    "revision": "d8e6feca56325fc6a4603bf2fcc1499b",
+    "url": "/modular-layouts/index.html"
+  }
+];
